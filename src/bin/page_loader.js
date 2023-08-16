f4397f657774507c59566145b5fb8602e6f5242d
lmorningstar <jaster_ru@outlook.com>
2023-08-16 21:06:13 +0300

    Fix page_loader commander

--- src/bin/page_loader.js
+++ src/bin/page_loader.js
@@ -1,50 +1,22 @@
-import { Command } from 'commander';
-import chalk from 'chalk';
-import Listr from 'listr';
-import debug from 'debug';
+import { program } from 'commander';
 
-import loader from '../index.js';
-
-const program = new Command();
-
-const debugError = debug('page-loader:error');
+import pageLoader from '../index.js';
 
 program
-  .version('1.0.0')
-  .arguments('<address>')
-  .action((address) => {
-    const taskFunc = (url, func, config) => {
-      let result;
-      const tasks = new Listr([
-        {
-          title: `Loading file '${url}'`,
-          task: (ctx, task) => func(url, config)
-            .then((data) => {
-              result = data;
-              return true;
-            })
-            .catch(err => task.skip(`Fail load '${url}'. ${err.message}`)),
-        },
-      ]);
-      return tasks.run()
-        .then(() => result);
-    };
-    loader(address, program.output, taskFunc)
-      .then(() => process.exit(0))
+  .name('page-loader')
+  .version('1.0.0', '-v, --version', 'output the version number')
+  .description('Load the page from url')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-o, --output [pathToFile]', 'output path to file', process.cwd())
+  .arguments('<url>')
+  .action((url, option) => {
+    pageLoader(url, option.output)
+      .then(({ filepath }) => {
+        console.log(filepath);
+      })
       .catch((err) => {
-        console.log(err);
-        debugError(err);
-        switch (err.code) {
-          case 'ENOTFOUND':
-            console.error(chalk.red(`404: page '${err.config.url}' not found.`));
-            break;
-          default:
-            console.error(chalk.red(err.message));
-        }
+        console.error(err.toString());
         process.exit(1);
       });
-  })
-  .description('Download page')
-  .option('-o, --output [path]', 'output path');
-
+  });
 program.parse(process.argv);
