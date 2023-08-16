89cc70b37e115b272a0ae06eb796f211cc918639
lmorningstar <jaster_ru@outlook.com>
2023-08-16 17:29:53 +0300

    Fix babel

---
+++ dist/lib/sourceList.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = {
+  link: 'href',
+  script: 'src',
+  img: 'src'
+};
+exports.default = _default;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6WyJsaW5rIiwic2NyaXB0IiwiaW1nIiwiZXhwb3J0cyIsImRlZmF1bHQiLCJfZGVmYXVsdCJdLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9saWIvc291cmNlTGlzdC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XHJcbiAgbGluazogJ2hyZWYnLFxyXG4gIHNjcmlwdDogJ3NyYycsXHJcbiAgaW1nOiAnc3JjJ1xyXG59XHJcbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O2VBQWU7RUFDYkEsSUFBSSxFQUFFLE1BQU07RUFDWkMsTUFBTSxFQUFFLEtBQUs7RUFDYkMsR0FBRyxFQUFFO0FBQ1AsQ0FBQztBQUFBQyxPQUFBLENBQUFDLE9BQUEsR0FBQUMsUUFBQSJ9
